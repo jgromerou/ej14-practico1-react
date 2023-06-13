259ccdf64f1543641c98cbc704598a561a7a1075
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-06-13 17:31:33 -0300

    agregamos el componente base de CrearBlog y el editarBlog

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -2,7 +2,7 @@ import { Button } from 'react-bootstrap';
 import error from '../../assets/error404.jpg';
 const Error404 = () => {
   return (
-    <section className="mainSection text-center ">
+    <section className="mainSection text-center mt-3 mb-1">
       <img src={error} alt="error 404" className="error404" />
       <div>
         <Button variant="primary" className="my-3">
---
+++ src/components/views/blog/CrearBlog.jsx
@@ -0,0 +1,9 @@
+const CrearBlog = () => {
+  return (
+    <section className="mainSection text-center my-3">
+      <h2>Aqui va el crear nuevo blog</h2>
+    </section>
+  );
+};
+
+export default CrearBlog;
