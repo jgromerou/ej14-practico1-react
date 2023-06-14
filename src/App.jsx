8fc64fcb21e2fb59343d6f3e54bccb7b170effc2
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-06-14 19:34:49 -0300

    se hizo dos columnas de Sidebar y MainAdministrador para pantallas medianas y grandes y una sola columna cuando es pantalla chica

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,21 @@ import './App.css';
 import Menu from './components/common/Menu';
 import Footer from './components/common/Footer';
 // import Inicio from './components/views/Inicio';
-// import Administrador from './components/views/Administrador';
+import Administrador from './components/views/Administrador';
 // import Error404 from './components/views/Error404';
 
 // import CrearBlog from './components/views/blog/CrearBlog';
-import EditarBlog from './components/views/blog/EditarBlog';
+// import EditarBlog from './components/views/blog/EditarBlog';
 
 function App() {
   return (
     <>
       <Menu></Menu>
       {/* <Inicio></Inicio> */}
-      {/* <Administrador></Administrador> */}
+      <Administrador></Administrador>
       {/* <Error404></Error404> */}
       {/* <CrearBlog></CrearBlog> */}
-      <EditarBlog></EditarBlog>
+      {/* <EditarBlog></EditarBlog> */}
       <Footer></Footer>
     </>
   );
--- src/components/views/Administrador.jsx
+++ src/components/views/Administrador.jsx
@@ -1,8 +1,18 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import Sidebar from '../common/Sidebar';
+
 const Administrador = () => {
   return (
-    <section className="mainSection text-center my-3">
-      <h2>Aqui va Administrador</h2>
-    </section>
+    <Container className="mainSection my-3">
+      <Row className="g-3 mb-3">
+        <Sidebar />
+        <Col md={9}>
+          <aside className="flex-grow-aside">
+            <h1 className="fw-bold mb-3 p-2">Main</h1>
+          </aside>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
