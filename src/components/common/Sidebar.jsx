1df0e61402b034d3eef998d7de68e6302764ae57
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-06-15 12:54:52 -0300

    se hizo el maquetado del formulario Crear Receta y se acomodó los botones para pantallas chicas y el alto de los textareas

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,12 +1,12 @@
 import { Col, Button } from 'react-bootstrap';
-const Sidebar = () => {
+const Sidebar = ({ tituloBoton = 'Nueva Receta' }) => {
   return (
     <>
       <Col md={3}>
         <aside className="flex-grow-aside">
           <h3 className="fw-bold mb-3 mt-2">Hola: usuario</h3>
           <Button variant="primary" className="w-100 d-sm-block mt-1">
-            Nueva Receta
+            {tituloBoton}
           </Button>
         </aside>
       </Col>
