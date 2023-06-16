06390f79a2b6008a3981f8518ab49cf9ac5d19e2
belen ramos <ramos.emiliabelen@gmail.com>
2023-06-16 18:18:26 -0300

    Creamos las rutas y creamos el archivo .env

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,41 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Menu from './components/common/Menu';
-import Footer from './components/common/Footer';
-// import Inicio from './components/views/Inicio';
-// import Administrador from './components/views/Administrador';
-// import Error404 from './components/views/Error404';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Menu from "./components/common/Menu";
+import Footer from "./components/common/Footer";
+import Inicio from "./components/views/Inicio";
+import Administrador from "./components/views/Administrador";
+import Error404 from "./components/views/Error404";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import CrearReceta from './components/views/receta/CrearReceta';
-// import EditarReceta from './components/views/receta/EditarReceta';
+import CrearReceta from "./components/views/receta/CrearReceta";
+import EditarReceta from "./components/views/receta/EditarReceta";
 
 function App() {
   return (
     <>
+      <BrowserRouter>
       <Menu></Menu>
-      {/* <Inicio></Inicio> */}
-      {/* <Administrador></Administrador> */}
-      {/* <Error404></Error404> */}
-      <CrearReceta></CrearReceta>
-      {/* <EditarReceta></EditarReceta> */}
-      <Footer></Footer>
+        <Routes>
+          <Route exact path="/" element={<Inicio />}></Route>
+          <Route
+            exact
+            path="/administrador"
+            element={<Administrador />}
+          ></Route>
+          <Route
+            exact
+            path="/administrador/crear-receta"
+            element={<CrearReceta />}
+          ></Route>
+          <Route
+            exact
+            path="/administrador/editar-receta"
+            element={<EditarReceta />}
+          ></Route>
+          <Route exact path="*" element={<Error404 />}></Route>
+        </Routes>
+        <Footer></Footer>
+      </BrowserRouter>
     </>
   );
 }
