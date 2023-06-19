cf1846ff5a2316850115eee061a3b39417cd8080
belen ramos <ramos.emiliabelen@gmail.com>
2023-06-19 11:17:45 -0300

    se hizo la logica de las cards del inicio

--- src/components/views/receta/CardReceta.jsx
+++ src/components/views/receta/CardReceta.jsx
@@ -1,16 +1,13 @@
 import { Col, Card, Button } from "react-bootstrap";
 
-const CardReceta = () => {
+const CardReceta = ({ receta }) => {
   return (
     <Col md={4} lg={3} className="mb-3">
       <Card>
-        <Card.Img
-          variant="top"
-          src="https://images.pexels.com/photos/7601435/pexels-photo-7601435.jpeg?auto=compress&cs=tinysrgb&w=600"
-        />
+        <Card.Img variant="top" src={receta.imagen} />
         <Card.Body>
-          <Card.Title>ASADITO</Card.Title>
-          <Card.Text>Categoria: Salado</Card.Text>
+          <Card.Title>{receta.nombreReceta}</Card.Title>
+          <Card.Text>Categoria: {receta.categoria}</Card.Text>
           <Button variant="primary">Ver Mas</Button>
         </Card.Body>
       </Card>
