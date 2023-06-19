0ee1fbaba47042676a69195d2f55d80708d5e75c
belen ramos <ramos.emiliabelen@gmail.com>
2023-06-19 09:38:55 -0300

    se creo el maquetado de inicio

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -1,7 +1,25 @@
+import { Container, Row } from "react-bootstrap";
+// import CardReceta from "./receta/CardReceta";
+
 const Inicio = () => {
   return (
-    <section className="mainSection text-center my-3">
-      <h2>Aqui va Inicio</h2>
+    <section className="mainSection">
+      <img
+        className="banner imgbanner"
+        src="https://images.pexels.com/photos/5737464/pexels-photo-5737464.jpeg"
+        alt="fondo receta"
+      />
+      <Container>
+        <h1 className="display-4 my-2">Nuestras Recetas</h1>
+        <hr />
+        {/* <Row>
+          <CardReceta></CardReceta>
+          <CardReceta></CardReceta>
+          <CardReceta></CardReceta>
+          <CardReceta></CardReceta>
+          <CardReceta></CardReceta>
+        </Row> */}
+      </Container>
     </section>
   );
 };
