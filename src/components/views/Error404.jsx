5b16c00b97fe0ded0c241171ca2e7c9a54e8f7e2
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-06-20 11:10:29 -0300

    se agregó el direccionamiento del botón de volver al inicio, se ajustó imagen y boton para pantallas pequeñas, y se puso fondo color blanco

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,13 +1,16 @@
-import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import error from '../../assets/error404.jpg';
 const Error404 = () => {
   return (
-    <section className="mainSection text-center mt-3 mb-1">
+    <section className="mainSection text-center  mb-1 bg-light">
       <img src={error} alt="error 404" className="error404" />
       <div>
-        <Button variant="primary" className="my-3">
-          Volver al inicio
-        </Button>
+        <Link
+          to="/"
+          className="btn btn-primary d-block d-sm-inline-block my-3 mx-3"
+        >
+          Volver al Inicio
+        </Link>
       </div>
     </section>
   );
