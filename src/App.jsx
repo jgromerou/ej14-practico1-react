dba8537d6e0f798dce5849c7814f7073cca5a427
belen ramos <ramos.emiliabelen@gmail.com>
2023-06-20 09:41:37 -0300

    se termino el login de usuario

--- src/App.jsx
+++ src/App.jsx
@@ -10,28 +10,44 @@ import DetalleReceta from "./components/views/DetalleReceta";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RutasPrivadas from "./components/routes/RutasPrivadas";
 import RutasAdministrador from "./components/routes/RutasAdministrador";
-
+import { useState } from "react";
 
 function App() {
+  const usuario = JSON.parse(sessionStorage.getItem("usuario")) || {};
+  const [usuarioLogueado, setUsuarioLogueado] = useState(usuario);
   return (
     <>
       <BrowserRouter>
-    <Menu></Menu>
-      <Routes>
-        <Route exact path="/" element={<Inicio></Inicio>}></Route>
-        <Route exact path="/registro" element={<Registro></Registro>}></Route>
-        <Route exact path="/login" element={<Login></Login>}></Route>
-        <Route exact path="/detalle" element={<DetalleReceta></DetalleReceta>}></Route>
-        <Route path="/administrador/*" element={
-          <RutasPrivadas>
-            <RutasAdministrador></RutasAdministrador>
-          </RutasPrivadas>
-        }></Route>
-        
-        <Route path="*" element={<Error404></Error404>}></Route>
-      </Routes>
-      <Footer></Footer>
-    </BrowserRouter>
+        <Menu
+          usuarioLogueado={usuarioLogueado}
+          setUsuarioLogueado={setUsuarioLogueado}
+        ></Menu>
+        <Routes>
+          <Route exact path="/" element={<Inicio></Inicio>}></Route>
+          <Route exact path="/registro" element={<Registro></Registro>}></Route>
+          <Route
+            exact
+            path="/login"
+            element={<Login setUsuarioLogueado={setUsuarioLogueado}></Login>}
+          ></Route>
+          <Route
+            exact
+            path="/detalle"
+            element={<DetalleReceta></DetalleReceta>}
+          ></Route>
+          <Route
+            path="/administrador/*"
+            element={
+              <RutasPrivadas>
+                <RutasAdministrador></RutasAdministrador>
+              </RutasPrivadas>
+            }
+          ></Route>
+
+          <Route path="*" element={<Error404></Error404>}></Route>
+        </Routes>
+        <Footer></Footer>
+      </BrowserRouter>
     </>
   );
 }
